b0a0c92496d2c8c1b939d3a292e7c6a5575de136
Jimmy Tang <jtang@tchpc.tcd.ie>
2011-05-30 13:27:54 +0100

    really expire accounts in example

--- expiredb/expireprojects.sh
+++ expiredb/expireprojects.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-recsel -C -R project -t project -e "enddate << '`date -I`'" projects.rec
+projects=$(recsel -C -R project -t project -e "enddate << '`date -I`'" projects.rec)
+
+for i in $projects
+do 
+	sbank project expire -a $i -c $(sbank cluster list) 
+done
